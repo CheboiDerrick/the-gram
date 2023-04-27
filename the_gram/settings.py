c821d82d2034e921d6a666efa3c354717bff4233
Cheboi Derrick <derrick.cheboi@student.moringaschool.com>
2023-04-27 14:22:18 +0300

    Update settings.py

--- the_gram/settings.py
+++ the_gram/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-6kf%eup%aajxjy8_%&)+70e0)+iy$scid3h$#7(&il^r&qyii4'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
