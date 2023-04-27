73f3f81c5de1d8321cf7b9f68916b0036134111c
Cheboi Derrick <derrick.cheboi@student.moringaschool.com>
2023-04-27 14:18:47 +0300

    Update settings.py for the database setup for vercel

--- the_gram/settings.py
+++ the_gram/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'the_gram.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv('POSTGRES_URL'),
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT),
+    }
 }
 
 
